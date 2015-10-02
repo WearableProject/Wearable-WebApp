9feb4662d709117c81c0e85399a56b8b04151435
Godley <charlotte.godley@hotmail.co.uk>
2015-10-02 17:27:38 +0100

    Issue fixfix #13

--- BlocklyDuino/blockly/generators/arduino/pearl.js
+++ BlocklyDuino/blockly/generators/arduino/pearl.js
@@ -3,6 +3,9 @@ goog.provide('Blockly.Arduino.Adaband');
 goog.require('Blockly.Arduino');
 
 Blockly.Arduino.set_brightness = function() {
-  // Text value.
-  return "pearl.off();\n";
+  var brightnessVal = this.getFieldValue("NUMBER");
+  Blockly.Arduino.definitions_['define_pearl'] = "Pearl pearl;\n";
+  Blockly.Arduino.setups_['setup_pearl'] = "pearl = Pearl();\n";
+  var code ="pearl.setBrightness("+brightnessVal+");\n";
+  return code;
 };
